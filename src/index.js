5883ba25e285e10e63f7585574bb8a1dfffe7aa3
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2018-03-05 22:57:28 -1000

    Add index.js exports

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-// export checkout from './checkout'
+import { name as pkgName } from '../package.json'
+
+// Reducer
+export checkoutReducer from './checkout'
+
+// Action types and creators
+export * from './checkout'
+
+// Middleware
+export const createShopifyMiddleware = client => _store => next => action => {
+  if (action.type.startsWith(pkgName + '/'))
+    action.meta = { ...action.meta, client }
+
+  return next(action)
+}

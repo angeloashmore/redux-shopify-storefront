bbee8588995308505c6dbbbe575977205ba6ef56
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2018-03-05 23:19:09 -1000

    Restructure sagas and index.js

--- src/checkout/sagas.js
+++ src/checkout/sagas.js
@@ -10,7 +10,7 @@ import {
 // NOTE: client undefined. Need to implement a way to pass the Shopify domain
 // and storefront access token.
 
-function* createCheckoutSaga() {
+function* createCheckoutSaga({ client }) {
   const checkoutCreateInput = yield select(state => pick(state, ['lineItems']))
 
   const checkout = yield call(client.checkout.create)
@@ -19,15 +19,15 @@ function* createCheckoutSaga() {
   yield put(setWebUrl(checkout.webUrl))
 }
 
-function* updateCheckoutSaga() {
+function* updateCheckoutSaga({ client }) {
   const checkoutId = yield select(state => state.checkout.checkoutId)
 
   if (!checkoutId) yield call(createCheckoutSaga)
 }
 
-const sagas = [
+const saga = [
   takeEvery(CREATE_CHECKOUT, createCheckoutSaga),
   takeEvery(UPDATE_CHECKOUT, updateCheckoutSaga),
 ]
 
-export default sagas
+export default saga
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+import { all } from 'redux-saga/effects'
+import checkoutSagas from './checkout/sagas'
 import { name as pkgName } from '../package.json'
-
-// Reducer
-export checkoutReducer from './checkout'
 
 // Action types and creators
 export * from './checkout'
+
+// Reducers
+export { default as checkoutReducer } from './checkout'
 
 // Middleware
 export const createShopifyMiddleware = client => _store => next => action => {
@@ -12,4 +14,9 @@ export const createShopifyMiddleware = client => _store => next => action => {
     action.meta = { ...action.meta, client }
 
   return next(action)
+}
+
+// Saga
+export function* shopifySaga() {
+  yield all([...checkoutSagas])
 }
